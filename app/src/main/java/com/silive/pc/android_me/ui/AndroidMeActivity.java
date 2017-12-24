11a5758db28f904659c02c1cca1457b860170313
jadaungeetanjali <jadaungeetanjali@gmail.com>
2017-12-24 15:26:36 +0530

    interface communication and communication between activities using intents

--- app/src/main/java/com/silive/pc/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/silive/pc/android_me/ui/AndroidMeActivity.java
@@ -23,7 +23,11 @@ public class AndroidMeActivity extends AppCompatActivity {
 
             // Set the list of image id's for the head fragment and set the position to the second image in the list
             headFragment.setImageIds(AndroidImageAssets.getHeads());
-            headFragment.setListIndex(1);
+
+            // Get the correct index to access in the array of head images from the intent
+            // Set the default value to 0
+            int headIndex = getIntent().getIntExtra("headIndex", 0);
+            headFragment.setListIndex(headIndex);
 
             // add the fragment to its container using FragmentManager and its transaction
             android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
@@ -34,12 +38,16 @@ public class AndroidMeActivity extends AppCompatActivity {
             //create and display the head and body BodyPartFragemnt
             BodyPartFragment bodyFragment = new BodyPartFragment();
             bodyFragment.setImageIds(AndroidImageAssets.getBodies());
+            int bodyIndex = getIntent().getIntExtra("bodyIndex", 0);
+            bodyFragment.setListIndex(bodyIndex);
             fragmentManager.beginTransaction()
                     .add(R.id.body_container, bodyFragment)
                     .commit();
 
             BodyPartFragment legFragment = new BodyPartFragment();
             legFragment.setImageIds(AndroidImageAssets.getLegs());
+            int legIndex = getIntent().getIntExtra("legIndex", 0);
+            legFragment.setListIndex(legIndex);
             fragmentManager.beginTransaction()
                     .add(R.id.leg_container, legFragment)
                     .commit();
--- app/src/main/java/com/silive/pc/android_me/ui/MasterListFragment.java
+++ app/src/main/java/com/silive/pc/android_me/ui/MasterListFragment.java
@@ -1,11 +1,13 @@
 package com.silive.pc.android_me.ui;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 import com.silive.pc.android_me.R;
@@ -18,6 +20,20 @@ import com.silive.pc.android_me.data.AndroidImageAssets;
 // The list appears as a grid of images
 public class MasterListFragment extends Fragment {
 
+    // Define a new interface OnImageClickListener that triggers a callback in the host activity
+    OnImageClickListener mCallBack;
+
+    // OnImageClickListener interface, calls a method in the host activity named onImageSelected
+    public interface OnImageClickListener{
+        void onImageSelected(int position);
+    }
+
+    // Override onAttach to make sure that the container activity has implemented the callback
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mCallBack = (OnImageClickListener) context;
+    }
 
     public MasterListFragment() {
         // Required empty public constructor
@@ -34,6 +50,15 @@ public class MasterListFragment extends Fragment {
 
         MasterListAdapter mAdapter = new MasterListAdapter(getContext(), AndroidImageAssets.getAll());
         gridView.setAdapter(mAdapter);
+
+        // Set a click listener on the gridView and trigger the callback onImageSelected when an item is clicked
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Trigger the callback method and pass in the position that was clicked
+                mCallBack.onImageSelected(position);
+            }
+        });
         return rootView;
     }
 
